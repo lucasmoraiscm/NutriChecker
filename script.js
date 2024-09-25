e091b6a407addc1c952341ea113a16c004e20717
Lucas Morais <108703672+lucasmoraiscm@users.noreply.github.com>
2024-09-25 19:56:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,99 +1,111 @@
-function calcular() {
-    var getCarb = document.querySelector('input#quantCarb')
-    var quantCarb = Number(getCarb.value)
+function calcular(){
+    let getquantPorçG = document.querySelector('input#quantPorçG')
+    let quantPorçG = Number(getquantPorçG.value)
 
-    var getProt = document.querySelector('input#quantProt')
-    var quantProt = Number(getProt.value)
+    let getquantPorçML = document.querySelector('input#quantPorçML')
+    let quantPorçML = Number(getquantPorçML.value)
 
-    var getGordTot = document.querySelector('input#quantGordTot')
-    var quantGordTot = Number(getGordTot.value)
+    let qtd_calorias = calcular_calorias()
+    let resultadoAçuAdi = verificar_acucares_adicionados(quantPorçG, quantPorçML)
+    let resultadoGordSat = verificar_gorduras_saturadas(quantPorçG, quantPorçML)
+    let resultadoSodio = verificar_sodio(quantPorçG, quantPorçML)
 
-    var getAçuAdiG = document.querySelector('input#quantAçuAdiG')
-    var quantAçuAdiG = Number(getAçuAdiG.value)
+    textoResultado.innerHTML = `<strong>O alimento possui ${qtd_calorias.toFixed(0)} Kcal</strong>`
+    textoAçuAdi.innerHTML = `<strong>${resultadoAçuAdi}</strong>`
+    textoGordSat.innerHTML = `<strong>${resultadoGordSat}</strong>`
+    textoSodio.innerHTML = `<strong>${resultadoSodio}</strong>`
+}
 
-    var getAçuAdiML = document.querySelector('input#quantAçuAdiML')
-    var quantAçuAdiML = Number(getAçuAdiML.value)
+function calcular_calorias(){
+    let getCarb = document.querySelector('input#quantCarb')
+    let quantCarb = Number(getCarb.value)
 
-    var getGordSatG = document.querySelector('input#quantGordSatG')
-    var quantGordSatG = Number(getGordSatG.value)
+    let getProt = document.querySelector('input#quantProt')
+    let quantProt = Number(getProt.value)
 
-    var getGordSatML = document.querySelector('input#quantGordSatML')
-    var quantGordSatML = Number(getGordSatML.value)
+    let getGordTot = document.querySelector('input#quantGordTot')
+    let quantGordTot = Number(getGordTot.value)
 
-    var getSodioG = document.querySelector('input#quantSodioG')
-    var quantSodioG = Number(getSodioG.value)
+    return (quantCarb * 4) + (quantGordTot * 9) + (quantProt * 4)
+}
 
-    var getSodioML = document.querySelector('input#quantSodioML')
-    var quantSodioML = Number(getSodioML.value)
+function verificar_acucares_adicionados(quantPorçG, quantPorçML){
+    let getAçuAdiG = document.querySelector('input#quantAçuAdiG')
+    let quantAçuAdiG = Number(getAçuAdiG.value)
 
-    var getquantPorçG = document.querySelector('input#quantPorçG')
-    var quantPorçG = Number(getquantPorçG.value)
+    let getAçuAdiML = document.querySelector('input#quantAçuAdiML')
+    let quantAçuAdiML = Number(getAçuAdiML.value)
 
-    var getquantPorçML = document.querySelector('input#quantPorçML')
-    var quantPorçML = Number(getquantPorçML.value)
+    let limite_acucar_adicionado_g = (15 * quantPorçG) / 100
+    let limite_acucar_adicionado_ml = (7.5 * quantPorçML) / 100
 
-    var cal = ((quantCarb*4)+(quantGordTot*9)+(quantProt*4))
-
-    var valorAçuAdiG = ((15*quantPorçG) / 100)
-    var valorAçuAdiML = ((7.5*quantPorçML) / 100)
-
-    var valorGordSatG = ((6*quantPorçG) / 100)
-    var valorGordSatML = ((3*quantPorçML) / 100)
-
-    var valorSodioG = ((600*quantPorçG) / 100)
-    var valorSodioML = ((300*quantPorçML) / 100)
-
-    textoResultado.innerHTML = `<strong>O alimento possui ${cal.toFixed(0)} Kcal</strong>`
-
-    if (quantPorçG != 0 && quantPorçML != 0) {
-        textoAçuAdi.innerHTML = `<strong>Informe somente uma porção!</strong>`
-    } else {
-        if (quantAçuAdiG >= valorAçuAdiG && quantAçuAdiG != 0) {
-            textoAçuAdi.innerHTML = `<strong>Alta quantidade de Açúcares Adicionados!</strong>`
-        } else if (quantAçuAdiG < valorAçuAdiG && quantAçuAdiG != 0) {
-            textoAçuAdi.innerHTML = `<strong>Quantidade razoável de Açúcares Adicionados</strong>`
-        } else if (quantAçuAdiG == 0 && quantAçuAdiML != 0) {
-            if (quantAçuAdiML >= valorAçuAdiML && quantAçuAdiML != 0) {
-                textoAçuAdi.innerHTML = `<strong>Alta quantidade Açúcares Adicionados!</strong>`
-            } else if (quantAçuAdiML < valorAçuAdiML && quantAçuAdiML != 0) {
-                textoAçuAdi.innerHTML = `<strong>Quantidade razoável de Açúcares Adicionados</strong>`
-            } else {
-                textoAçuAdi.innerHTML = ``
-            }
-        } else {
-            textoAçuAdi.innerHTML = ``
+    if (quantPorçG > 0 && quantPorçML == 0){
+        if (quantAçuAdiG >= limite_acucar_adicionado_g) {
+            return 'Alta quantidade de Açúcares Adicionados!'
+        }else{
+            return 'Quantidade razoável de Açúcares Adicionados'
         }
-
-        if (quantGordSatG >= valorGordSatG && quantGordSatG != 0) {
-            textoGordSat.innerHTML = `<strong>Alta quantidade de Gorduras Saturadas!</strong>`
-        } else if (quantGordSatG < valorGordSatG && quantGordSatG != 0) {
-            textoGordSat.innerHTML = `<strong>Quantidade razoável de Gorduras Saturadas</strong>`
-        } else if (quantGordSatG == 0 && quantGordSatML != 0) {
-            if (quantGordSatML >= valorGordSatML && quantGordSatML != 0) {
-                textoGordSat.innerHTML = `<strong>Alta quantidade de Gorduras Saturadas!</strong>`
-            } else if (quantGordSatML < valorGordSatML && quantGordSatML != 0) {
-                textoGordSat.innerHTML = `<strong>Quantidade razoável de Gorduras Saturadas</strong>`
-            } else {
-                textoGordSat.innerHTML = ``
-            }
-        }else {
-            textoGordSat.innerHTML = ``
-        }    
-
-        if (quantSodioG >= valorSodioG && quantSodioG != 0) {
-            textoSodio.innerHTML = `<strong>Alta quantidade de Sódio!</strong>`
-        } else if (quantSodioG < valorSodioG && quantSodioG != 0) {
-            textoSodio.innerHTML = `<strong>Quantidade razoável de Sódio</strong>`
-        } else if (quantSodioG == 0 && quantSodioML != 0) {
-            if (quantSodioML >= valorSodioML && quantSodioML != 0) {
-                textoSodio.innerHTML = `<strong> Alta quantidade de Sódio!</strong>`
-            } else if (quantSodioML < valorSodioML && quantSodioML != 0) {
-                textoSodio.innerHTML = `<strong>Quantidade razoável de Sódio</strong>`
-            } else {
-                textoSodio.innerHTML = ``
-            }
-        }else {
-            textoSodio.innerHTML = ``
+    }else if (quantPorçG == 0 && quantPorçML > 0){
+        if (quantAçuAdiML >= limite_acucar_adicionado_ml) {
+            return 'Alta quantidade Açúcares Adicionados!'
+        }else{
+            return 'Quantidade razoável de Açúcares Adicionados'
         }
+    }else{
+        return ''
+    }
+}
+
+function verificar_gorduras_saturadas(quantPorçG, quantPorçML){
+    let getGordSatG = document.querySelector('input#quantGordSatG')
+    let quantGordSatG = Number(getGordSatG.value)
+
+    let getGordSatML = document.querySelector('input#quantGordSatML')
+    let quantGordSatML = Number(getGordSatML.value)
+
+    let limite_gorduras_saturadas_g = (6 * quantPorçG) / 100
+    let limite_gorduras_saturadas_ml = (3 * quantPorçML) / 100
+
+    if (quantPorçG > 0 && quantPorçML == 0){
+        if (quantGordSatG >= limite_gorduras_saturadas_g) {
+            return 'Alta quantidade de Gorduras Saturadas!'
+        }else{
+            return 'Quantidade razoável de Gorduras Saturadas'
+        }
+    }else if (quantPorçG == 0 && quantPorçML > 0){
+        if (quantGordSatML >= limite_gorduras_saturadas_ml) {
+            return 'Alta quantidade Gorduras Saturadas!'
+        }else{
+            return 'Quantidade razoável de Gorduras Saturadas'
+        }
+    }else{
+        return ''
+    }
+}
+
+function verificar_sodio(quantPorçG, quantPorçML){
+    let getSodioG = document.querySelector('input#quantSodioG')
+    let quantSodioG = Number(getSodioG.value)
+
+    let getSodioML = document.querySelector('input#quantSodioML')
+    let quantSodioML = Number(getSodioML.value)
+
+    let limite_sodio_g = (600 * quantPorçG) / 100
+    let limite_sodio_ml = (300 * quantPorçML) / 100
+
+    if (quantPorçG > 0 && quantPorçML == 0){
+        if (quantSodioG >= limite_sodio_g) {
+            return 'Alta quantidade de Sódio!'
+        }else{
+            return 'Quantidade razoável de Sódio'
+        }
+    }else if (quantPorçG == 0 && quantPorçML > 0){
+        if (quantSodioML >= limite_sodio_ml) {
+            return 'Alta quantidade Sódio!'
+        }else{
+            return 'Quantidade razoável de Sódio'
+        }
+    }else{
+        return ''
     }
 }
